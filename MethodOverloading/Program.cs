4eee0a44eb971c60a2f8163991dcf5adf68e4c2e
kendall chapman <jkc4310@gmail.com>
2023-07-20 12:14:38 -0400

    Method overloading project

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,52 @@
 {
     public class Program
     {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool bool1)
+        {
+            var sum = 0;
+            var response = "";
+
+            if (bool1)
+            {
+                sum = num1 + num2;
+                //if (sum == 1)
+                //{
+                //    response = $"{sum} dollar.";
+                //}
+                //else
+                //{
+               //     response = $"{sum} dollars.";
+               // }
+
+                response = (sum == 1) ? $"{sum} dollar." : $"{sum} dollars.";
+            }
+
+            return response; 
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(0, 1, true)); 
+
+            Console.WriteLine();
+
+            Console.WriteLine(Add(5, 13, true));
+
+            Console.WriteLine();
+
+            Console.WriteLine(Add(-6, 4, true));
+
+            Console.WriteLine();
         }
     }
 }
